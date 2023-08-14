dcc4d31800c9c1cc7aaf3625ec73669c91110c1d
Jiyong Kim <luke.jiyongkim@gmail.com>
2023-08-14 16:12:34 -0400

    completed lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,13 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", "w", encoding="UTF-8") as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", "a", encoding="UTF-8") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", encoding="UTF-8") as file:
+        return file.read()
+
